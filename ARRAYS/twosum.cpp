067d7da7e5e5324d18b00851aa6bd540536db175
nahidaa-parbeen <mansurisalina51@gmail.com>
2026-03-11 19:25:49 +0530

    Implement twoSumExists and twoSumIndices methods

---
+++ ARRAYS/twosum.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string twoSumExists(vector<int> arr, int target) {
+        int n = arr.size();
+    
+        vector<pair<int, int>> numsWithIndex;
+        for (int i = 0; i < n; i++) {
+            numsWithIndex.push_back({arr[i], i});
+        }
+  
+        sort(numsWithIndex.begin(), numsWithIndex.end());
+
+        int left = 0, right = n - 1;
+      
+        while (left < right) {
+            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+            if (sum == target) {
+                return "YES"; 
+            } else if (sum < target) {
+                left++;  
+            } else {
+                right--; 
+            }
+        }
+        return "NO";  
+    }
+    vector<int> twoSumIndices(vector<int> arr, int target) {
+        int n = arr.size();
+        vector<pair<int, int>> numsWithIndex;
+        for (int i = 0; i < n; i++) {
+            numsWithIndex.push_back({arr[i], i});
+        }
+        sort(numsWithIndex.begin(), numsWithIndex.end());
+
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+            if (sum == target) {
+                return {numsWithIndex[left].second, numsWithIndex[right].second};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return {-1, -1}; 
+    }
+};
