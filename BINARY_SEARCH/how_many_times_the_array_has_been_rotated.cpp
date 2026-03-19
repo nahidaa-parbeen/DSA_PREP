43050de3dd98930bca596e6969109f08f0251dd2
nahidaa-parbeen <mansurisalina51@gmail.com>
2026-03-20 00:49:09 +0530

    Implement function to find array rotation count

---
+++ BINARY_SEARCH/how_many_times_the_array_has_been_rotated.cpp
@@ -0,0 +1,13 @@
+int findRotations(vector<int>& arr) {
+        int low = 0;
+        int high = arr.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
