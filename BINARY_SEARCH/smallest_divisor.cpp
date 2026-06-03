0e0ad6657f92aae489722ea2980e1b8a88fa6f8c
nahidaa-parbeen <mansurisalina51@gmail.com>
2026-06-03 23:48:58 +0530

    Implement smallest divisor binary search solution

---
+++ BINARY_SEARCH/smallest_divisor.cpp
@@ -0,0 +1,26 @@
+class Solution{
+public:
+    int sumByD(vector<int>& arr, int div) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += ceil((double)num / div);
+        }
+        return sum;
+    }
+    int smallestDivisor(vector<int>& arr, int limit) {
+        if (arr.size() > limit) return -1;
+
+        int low = 1;
+        int high = *max_element(arr.begin(), arr.end());
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(arr, mid) <= limit) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
