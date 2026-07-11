5b0a6d68d223f18e622eeaa2e716d1d3cb3cd489
nahidaa-parbeen <mansurisalina51@gmail.com>
2026-07-11 20:24:41 +0530

    Add palindrome check for singly-linked list
    Implement a solution to check if a singly-linked list is a palindrome.

---
+++ LINKED LIST/palindrom_sll.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode *reverse_ll(ListNode *head){
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* newhead=reverse_ll(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
+        return newhead;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL ||head->next==NULL) return true;
+        ListNode* slow=head;
+        ListNode*fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newhead=reverse_ll(slow->next);
+        ListNode* first=head;
+        ListNode*second=newhead;
+        while(second!=NULL){
+            if(first->val!=second->val){
+                reverse_ll(newhead);
+                return false;
+            }
+            first=first->next;
+            second=second->next;
+        }
+        reverse_ll(newhead);
+        return true;
+    }
+};
